ef116a6895e89e8d4c6ff29ac7079cb72bda5fef
unlimitedgk <johannes.eder@icloud.com>
2026-01-21 11:21:29 +0100

    Changed button colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,19 +7,5 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        gold: {
-          500: "#D4AF37",
-          600: "#C9A227",
-        },
-        red: {
-          500: "#ED2939",
-          600: "#C9A227",
-        },
-      },
-    },
-  },
   plugins: [],
 } satisfies Config
